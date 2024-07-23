177357baa46444583b8ec19e69ec84dcaf65a90d
dphuong2103 <midouz.project@gmail.com>
2024-07-23 16:10:46 +0700

    second commit

--- blog-web-app/constants/api.ts
+++ blog-web-app/constants/api.ts
@@ -1,4 +1,5 @@
-export const api_url = "http://localhost:8081/api/v1";
+export const env = process.env.NODE_ENV;
+export const api_url = !(env === "production") ? process.env.API_URL_DEV : process.env.API_URL_PROD;
 export const blog_api_url = `${api_url}/blogs`;
 export const user_api_url = `${api_url}/users`;
 export const authenticate_api_url = `${api_url}/authenticate`;
