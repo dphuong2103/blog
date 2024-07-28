f26b2458da01ec644c59a668107074a1cc8b112f
dphuong2103 <midouz.project@gmail.com>
2024-07-28 15:52:24 +0700

    adjust service name

--- web/constants/api.ts
+++ web/constants/api.ts
@@ -5,7 +5,7 @@ if(env === "production" ){
 }
 export const api_url = !(env === "production")
   ? process.env.API_URL_DEV
-  : "http://blog-be:8080/api/v1";
+  : "http://api:8080/api/v1";
 export const blog_api_url = `${api_url}/blogs`;
 export const user_api_url = `${api_url}/users`;
 export const tag_api_url = `${api_url}/tags`;
